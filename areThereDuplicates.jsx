7ce5cd7754ffaf80bec1423da92c06a7316648fb
shelbyreese47 <shelbyreese47@gmail.com>
2021-11-11 10:32:18 -0700

    complete averagePair and areThereDuplicates

--- areThereDuplicates.jsx
+++ areThereDuplicates.jsx
@@ -1,7 +1,53 @@
 // accepts a variable number of arguments
 
+// I'm going to have a counter and if the key value is greater than 1- return true and break out of the loop
 
+function areThereDuplicates() {
+    // console.log(arguments)
+    let counter = {};
+    for (let i=0; i< arguments.length; i++){
+        // counter[arguments[i]] = ++counter[arguments[i]] || 1;
+        if (counter[arguments[i]]) return true;
+         counter[arguments[i]] = 1;
+        // console.log(counter)
 
-areThereDuplicates(1,2,3) //f
-areThereDuplicates(1,2,2) //t
-areThereDuplicates('a', 'b', 'c','a') //t
+    }
+    return false;
+}
+
+console.log(areThereDuplicates(1,2,3)) //f
+console.log(areThereDuplicates(1,2,2)) //t
+console.log(areThereDuplicates('a', 'b', 'c','a')) //t
+
+//His solution- frequency counter
+function areThereDuplicates() {
+  let collection = {}
+  for(let val in arguments){
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for(let key in collection){
+    if(collection[key] > 1) return true
+  }
+  return false;
+}
+
+// His solution- multiple pointers
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a,b) => a > b);
+  let start = 0;
+  let next = 1;
+  while(next < args.length){
+    if(args[start] === args[next]){
+        return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+
+// His solution one liner
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
---
+++ averagePair.jsx
@@ -0,0 +1,23 @@
+// true if there is a pair in the array that has an average of the param
+
+function averagePair(arr, value) {
+    if (arr.length < 2) return false;
+    let left = 0;
+    let right = 1;
+    while (right < arr.length){
+        let avg = (arr[left]+arr[right])/2;
+        // console.log(avg)
+        if(avg === value) return true
+        right ++;
+        if(right === arr.length -1){
+            left ++;
+            right = left +1;
+        }
+    }
+    return false;
+}
+
+console.log(averagePair([1,2,3],2.5)) //t
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) //t
+console.log(averagePair([-1,0,3,4,5,6],4.1)) //f
+console.log(averagePair([],4)) //f
